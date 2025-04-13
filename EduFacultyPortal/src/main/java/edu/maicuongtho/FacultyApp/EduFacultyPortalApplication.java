c211fb4154b564fd6c81278cca4d2ccca22fe2ae
Mai Cường Thọ <thomc@ntu.edu.vn>
2025-04-13 11:19:34 +0700

    Create new empty project

---
+++ EduFacultyPortal/src/main/java/edu/maicuongtho/FacultyApp/EduFacultyPortalApplication.java
@@ -0,0 +1,13 @@
+package edu.maicuongtho.FacultyApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduFacultyPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduFacultyPortalApplication.class, args);
+	}
+
+}
---
+++ EduFacultyPortal/src/test/java/edu/maicuongtho/FacultyApp/EduFacultyPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.maicuongtho.FacultyApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduFacultyPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
